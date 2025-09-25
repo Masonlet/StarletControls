f84ebf222986c159deb5801874972a6ee1a4bdc2
Masonlet <masonletoile@hotmail.com>
2025-09-24 20:00:36 -0400

    Extracted controls to ECS systems

--- inc/StarletControls/modelController.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "controller.hpp"
-struct Model;
-
-struct ModelController : public Controller<Model> {
-	void update(Model& data, const InputManager& input, float deltaTime) override;
-};
